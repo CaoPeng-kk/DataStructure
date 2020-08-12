bff062910d40d364243da678344d8e8d196e2992
CaoPeng-kk <729575125@qq.com>
2020-08-12 10:58:58 +0800

    优化冒泡排序

--- src/jan/secondweek/sort/PopSort.java
+++ src/jan/secondweek/sort/PopSort.java
@@ -9,7 +9,9 @@ public class PopSort {
 
     public static void main(String[] args) {
         int[] array = {2,4,7,0,3,7,1};
+        System.out.println(System.currentTimeMillis());
         popSort(array);
+        System.out.println(System.currentTimeMillis());
         for(int num : array) {
             System.out.print(num+" ");
         }
@@ -28,13 +30,13 @@ public class PopSort {
             for (int j = 0; j < array.length-1; j++) {
                 if (array[j] > array[j+1]) {
                     int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                     flag = false;
                 }
             }
             if (flag) {
-                break;
+                return;
             }
         }
     }
