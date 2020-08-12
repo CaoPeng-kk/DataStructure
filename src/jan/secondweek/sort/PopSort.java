6fbab501f24e5ab849a3f00625592e9510c7b362
CaoPeng-kk <729575125@qq.com>
2020-08-12 10:42:58 +0800

    优化冒泡排序

--- src/jan/secondweek/sort/PopSort.java
+++ src/jan/secondweek/sort/PopSort.java
@@ -23,13 +23,18 @@ public class PopSort {
      * @Date: 2020/8/4
      */
     public static void popSort(int[] array) {
+        boolean flag = true;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length-1; j++) {
                 if (array[j] > array[j+1]) {
                     int temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = temp;
+                    flag = false;
                 }
+            }
+            if (flag) {
+                break;
             }
         }
     }
